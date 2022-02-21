a5383a0d4a0f1da9dff57d83a89e629ff9424107
Raspberrek <54684949+Raspberrek@users.noreply.github.com>
2022-02-21 21:25:49 +0100

    Create mount_disks.sh

---
+++ mount_disks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+lsblk
+mount /dev/sda /home/user/plots1
+mount /dev/sdb /home/user/plots2
+mount /dev/sdc /home/user/plots3
+mount /dev/sdd /home/user/plots4
+mount /dev/sde /home/user/plots5
+mount /dev/sdf /home/user/plots6
+mount /dev/sdg /home/user/plots7
+lsblk
