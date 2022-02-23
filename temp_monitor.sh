726d956df750596b6a559fd8d5c2dc08a18f2428
Raspberrek <54684949+Raspberrek@users.noreply.github.com>
2022-02-23 20:31:22 +0100

    Create temp_monitor.sh

---
+++ temp_monitor.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+hddtemp /dev/sda
+printf "\n"
+hddtemp /dev/sdb
+printf "\n"
+hddtemp /dev/sdc
+printf "\n"
+hddtemp /dev/sdd
+printf "\n"
+hddtemp /dev/sde
+printf "\n"
+hddtemp /dev/sdf
+printf "\n"
+hddtemp /dev/sdg
+printf "\n"
+hddtemp /dev/sdh
+printf "\n"
+hddtemp /dev/sdi
+printf "\n"
+hddtemp /dev/sdj
+printf "\n"
