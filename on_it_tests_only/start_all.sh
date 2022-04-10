8e9aff93e229153b1167bb22ada8f2830b3dbe22
Raspberrek <54684949+Raspberrek@users.noreply.github.com>
2022-04-10 16:13:35 +0200

    Update start_all.sh

--- on_it_tests_only/start_all.sh
+++ on_it_tests_only/start_all.sh
@@ -1,51 +1,51 @@
 #!/bin/bash
 
-cd /usr/lib/266/
+cd /opt/
 . ./activate
 chia start farmer
 #chia show -s
 #fetching -e 11
 
-cd /usr/lib/266/silicoin-blockchain/
+cd /opt/silicoin-blockchain/
 . ./activate
 sit start farmer
 #sit show -s
 #fetching -e 11
 
-cd /usr/lib/266/hddcoin-blockchain/
+cd /opt/hddcoin-blockchain/
 . ./activate
 hddcoin start farmer
 #hddcoin show -s
 #fetching -e 11
 
-cd /usr/lib/266/flora-blockchain/
+cd /opt/flora-blockchain/
 . ./activate
 flora start farmer
 #flora show -s
 #fetching -e 11
 
-cd /usr/lib/266/stai-blockchain/
+cd /opt/stai-blockchain/
 . ./activate
 stai start farmer
 #stai show -s
 
-cd /usr/lib/266/flax-blockchain/
+cd /opt/flax-blockchain/
 . ./activate
 flax start farmer
 #fetching -e 11
 
-cd /usr/lib/266/maize-blockchain/
+cd /opt/maize-blockchain/
 . ./activate
 maize start farmer
 
-cd /usr/lib/266/greendoge-blockchain/
+cd /opt/greendoge-blockchain/
 . ./activate
 greendoge start farmer
 
-cd /usr/lib/266/gold-blockchain/
+cd /opt/gold-blockchain/
 . ./activate
 gold start farmer
 
-cd /usr/lib/266/profit-blockchain/
+cd /opt/profit-blockchain/
 . ./activate
 profit start farmer
