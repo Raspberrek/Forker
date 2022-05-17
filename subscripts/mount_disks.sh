141985d60b75d9ea634a2fc9819b6ab5f9de623d
Raspberrek <54684949+Raspberrek@users.noreply.github.com>
2022-05-17 12:10:10 +0200

    Update mount_disks.sh

--- subscripts/mount_disks.sh
+++ subscripts/mount_disks.sh
@@ -8,11 +8,11 @@
 # mkdir /home/opt/plots/plots6
 # mkdir /home/opt/plots/plots7
 
-mount /dev/sda1 /home/opt/plots/plots1
-mount /dev/sdb2 /home/opt/plots/plots2
-mount /dev/sdc1 /home/opt/plots/plots3
-mount /dev/sdd2 /home/opt/plots/plots4
-mount /dev/sde2 /home/opt/plots/plots5
-mount /dev/sdf2 /home/opt/plots/plots6
-mount /dev/sdg2 /home/opt/plots/plots7
+mount /dev/sda1 /opt/plots/plots1
+mount /dev/sdb2 /opt/plots/plots2
+mount /dev/sdc1 /opt/plots/plots3
+mount /dev/sdd2 /opt/plots/plots4
+mount /dev/sde2 /opt/plots/plots5
+mount /dev/sdf2 /opt/plots/plots6
+mount /dev/sdg2 /opt/plots/plots7
 
