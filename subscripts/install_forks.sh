faf3931d8567d01869518c34e6c9d38c24b5cd42
Raspberrek <iotmanage@gmail.com>
2022-06-09 14:08:13 +0300

    NEw install forks file

---
+++ subscripts/install_forks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd /opt/
+git clone https://github.com/Chia-Network/chia-blockchain.git -b latest --recurse-submodules
+git clone https://github.com/joker-network/joker-blockchain.git
+git clone https://github.com/Flora-Network/flora-blockchain.git
+git clone https://github.com/Petroleum-Network/petroleum-blockchain.git
+git clone https://github.com/Flax-Network/flax-blockchain.git
+git clone https://github.com/Maize-Network/maize-blockchain.git
+git clone https://github.com/STATION-I/stai-blockchain.git
+git clone https://github.com/GreenDoge-Network/greendoge-blockchain.git
+git clone https://github.com/GreenDoge-Network/greendoge-blockchain.git
+git clone https://github.com/silicoin-network/silicoin-blockchain.git
+git clone https://github.com/Stor-Network/stor-blockchain.git
+git clone https://github.com/BTCgreen-Network/btcgreen-blockchain.git
+git clone https://github.com/HDDcoin-Network/hddcoin-blockchain.git
+git clone https://github.com/denisio/seno-blockchain.git
+
